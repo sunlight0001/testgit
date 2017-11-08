183ca38e52a2441fd3febed61a805c934d423e38
sunlight <18310817576@163.com>
2017-11-08 22:00:40 +0800

    dev  commit

--- mygitstudy/src/git/study/MyFirstCommit.java
+++ mygitstudy/src/git/study/MyFirstCommit.java
@@ -9,6 +9,10 @@ public class MyFirstCommit {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("this is add in dev branch");
 	}
 
+	public void sys(){
+		System.out.println("dev");
+	}
 }
