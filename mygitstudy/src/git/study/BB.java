1bfca96462f06cdeec80bd87e92be9c056ffd102
sunlight <18310817576@163.com>
2018-02-20 15:04:24 +0800

    add  two class

---
+++ mygitstudy/src/git/study/BB.java
@@ -0,0 +1,9 @@
+package git.study;
+
+public class BB {
+
+    public static void main(String[] args) {
+		
+		System.out.println("this is bb class");
+	}
+}
---
+++ mygitstudy/src/git/study/CC.java
@@ -0,0 +1,9 @@
+package git.study;
+
+public class CC {
+
+    public static void main(String[] args) {
+		
+		System.out.println("this is cc class");
+	}
+}
